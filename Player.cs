b875226f888c95079d3c1825e01c56c26207f065
Henry Gillinger <henry.gillinger@ga.ntig.se>
2024-03-07 14:18:43 +0100

    pokfa

---
+++ Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using Raylib_cs;
+
+namespace slutP;
+
+
+public class Player : GameObject
+{
+    public float speed = 0.1f;
+    public Vector2 pos = new Vector2();
+    public Vector2 size = new Vector2(20, 20);
+
+    public override void Update()
+    {
+        if (Raylib.IsKeyDown(KeyboardKey.D))
+        {
+            pos.X += speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.A))
+        {
+            pos.X -= speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.S))
+        {
+            pos.Y += speed;
+        }
+        if (Raylib.IsKeyDown(KeyboardKey.W))
+        {
+            pos.Y -= speed;
+        }
+    }
+
+    public override void Draw()
+    {
+        Rectangle characterBox = new Rectangle(pos.X, pos.Y, size.X, size.Y);
+        Raylib.DrawRectangleRec(characterBox, Color.Red);
+    }
+
+
+}
+
+public class GameObject
+{
+    public virtual void Update() { }
+    public virtual void Draw() { }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Raylib_cs;
+using slutP;
+
+Raylib.InitWindow(800, 600, "BlACKBAbY");
+
+Player player = new();
+
+List<GameObject> gameObjects = new();
+
+gameObjects.Add(player);
+
+while (!Raylib.WindowShouldClose())
+{
+    foreach (var item in gameObjects)
+    {
+        item.Update();
+    }
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+
+    foreach (var item in gameObjects)
+    {
+        item.Draw();
+    }
+    Raylib.EndDrawing();
+}
+
+// STEG 1 = SPALTA UP VAD BESTÅR AV! FUNDERA VILKA OLIKA GREJER SOM FINNS MER. 
+// ANVÄND PAINT
+// STEG 2 = VILKA AV DEM HÄR SAKERNA ÄR VIKTIGTS
+
